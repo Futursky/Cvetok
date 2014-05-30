bd93e24c144a69e3238221e6216bd69967e13bb7
razix <yfuturskyi@yfuturskyi-To-be-filled-by-O-E-M.(none)>
2014-05-30 13:15:13 +0300

    Add dropbox

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,14 @@ class Image < ActiveRecord::Base
   MAX_FILESIZE = 50.megabytes
   belongs_to :imagable, :polymorphic => true
   delegate :path, :url, :content_type, :to => :photo
-  has_attached_file :photo, :styles => { :medium => "600x450#>", :thumb => "100x100>", :carousel => "1280x600#" }, :default_url => "/images/missing.png"
+  has_attached_file :photo,
+                    :styles => { :medium => "600x450#>",
+                                 :thumb => "100x100>",
+                                 :carousel => "1280x600#" },
+                    :default_url => "/images/missing.png",
+                    storage: :dropbox,
+                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :photo, :less_than => MAX_FILESIZE
+
 end
